f1bbc742dbeca8d055a1dff37c9c0336fb95514b
ChuongC <154713231+ChuongC@users.noreply.github.com>
2024-04-30 01:18:13 +0700

    Add files via upload
    HPBD My Love 2024

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Baby Cute';
+const giftUrl = '';
+const eventName = 'Happy Birthday';
+const titleCard = 'Dearest My Bà Nhà';
+
+const contentCard = 'Happy birthday, my baby cutie! You know, there is no word that can express my gratitude to you because you come into my life and make it cooler and cooler, and that is always the greatest thing for me. Our lovely and beautiful memories together will never stop, and a big life is waiting for us ahead. Hand in hand, we are going to go through it all. Not only for several days, months, or even years, but actually it is a life. I wish my world is you with my best regards: always have good luck, lots of joy and happiness, and always smile brightly. LOVE and MISS YOU BABY!';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
